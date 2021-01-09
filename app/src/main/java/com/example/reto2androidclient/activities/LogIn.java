9fe13c73f0aed398858b87c101b944d5751288d9
aitorfi <aitorfidalgo@gmail.com>
2021-01-09 16:22:13 +0100

    activity_home.xml implemented.

--- app/src/main/java/com/example/reto2androidclient/activities/LogIn.java
+++ app/src/main/java/com/example/reto2androidclient/activities/LogIn.java
@@ -2,15 +2,29 @@ package com.example.reto2androidclient.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.reto2androidclient.R;
 
 public class LogIn extends AppCompatActivity {
 
+    private Button buttonSignIn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        buttonSignIn = findViewById(R.id.buttonSignIn);
+        buttonSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LogIn.this, Home.class);
+                startActivity(intent);
+            }
+        });
     }
 }
