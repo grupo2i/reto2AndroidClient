b55200f6ab6e0c3572b7273410ca74edce66383f
marlock97 <44087045+marlock97@users.noreply.github.com>
2021-01-26 14:21:33 +0100

    Started Event activity.

---
+++ app/src/main/java/com/example/reto2androidclient/activities/EventActivity.java
@@ -0,0 +1,15 @@
+package com.example.reto2androidclient.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.reto2androidclient.R;
+import android.os.Bundle;
+
+public class EventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_event);
+    }
+}
